26aa9966d9d0c296eed2544eaab96e73b44bcfcd
Wonder <wonderdxk@gmail.com>
2020-05-13 00:21:22 -0400

    1.0.0
    Initialize project

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask import jsonify
+from flask import g
+
+from models import initialize
+from models import Course
+from models import DATABASE
+
+app = Flask(__name__)
+PORT = 8000
+DEBUG = True
+
+@app.before_request
+def before_request():
+    g.db = DATABASE
+    g.db.connect()
+
+@app.after_request
+def after_request(request):
+    g.db.close()
+    return request 
+
+@app.route('/codigo/api/v1.0/courses', methods=['GET'])
+def get_courses():
+    courses = Course.select()
+    courses = [course.to_json for course in courses]
+    return jsonify( courses )
+
+if __name__ == '__main__':
+    initialize()
+    app.run(port = PORT, debug = DEBUG)
